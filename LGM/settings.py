67b59416db78b9a52e56b4af69b05b5402d4b291
Shreya@3112 <shreyashgurav31@gmail.com>
2025-06-17 12:34:06 +0530

    new branch

--- LGM/settings.py
+++ LGM/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LGM',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '3306',
         # 'NAME': os.environ.get('LGM'),
