845bf033ae5de156f6bc341f015abc8900b55506
Ritesh Chavan <riteshchavannavy@gmail.com>
2025-06-18 14:37:34 +0530

    ii

--- LGM/settings.py
+++ LGM/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LGM',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'Sweety@71099',
         'HOST': 'localhost',
         'PORT': '3306',
         # 'NAME': os.environ.get('LGM'),
